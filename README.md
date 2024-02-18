5f6623fd8f1085675dadf8366d67889311195956
eugene <sail456852@icloud.com>
2024-02-18 17:32:05 +0800

    feat: Add OPENAPI 3

---
+++ README.md
@@ -0,0 +1,5 @@
+# URLs
+
+- http://localhost:8080/home
+- http://localhost:8080/v3/api-docs
+- http://localhost:8080/swagger-ui.html
--- src/main/java/eugene/controller/HomeController.java
+++ src/main/java/eugene/controller/HomeController.java
@@ -1,11 +1,11 @@
 package eugene.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeController {
-    @RequestMapping("/home")
+    @GetMapping("/home")
     public String home() {
         return "Welcome to Spring Boot";
     }
