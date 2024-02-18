1927f1ae012baa9bded9a981440f91064ae6cd92
eugene <sail456852@icloud.com>
2024-02-18 17:36:52 +0800

    feat: Check why is the compiler parameters option not needed?

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 - http://localhost:8080/home
 - http://localhost:8080/v3/api-docs
 - http://localhost:8080/swagger-ui.html
+- http://localhost:8080/greeting
+- http://localhost:8080/greeting?name=eugene
--- src/main/java/eugene/controller/HomeController.java
+++ src/main/java/eugene/controller/HomeController.java
@@ -1,6 +1,7 @@
 package eugene.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -8,5 +9,11 @@ public class HomeController {
     @GetMapping("/home")
     public String home() {
         return "Welcome to Spring Boot";
+    }
+
+    @GetMapping("/greeting")
+    public String greeting(@RequestParam(required = false, defaultValue = "World") String name) {
+        System.out.println("==== get greeting ====");
+        return "Hello, " + name;
     }
 }
