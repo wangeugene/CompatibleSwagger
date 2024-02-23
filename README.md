39fab0743478e21f0741e3ec92ed40e678d8f5a3
eugene <sail456852@icloud.com>
2024-02-23 18:24:16 +0800

    dependency: Upgrade to JDK 17 & Gradle 8.6 & SpringBoot 3.2.2

--- README.md
+++ README.md
@@ -1,7 +1,3 @@
 # URLs
 
 - http://localhost:8080/home
-- http://localhost:8080/v3/api-docs
-- http://localhost:8080/swagger-ui.html
-- http://localhost:8080/greeting
-- http://localhost:8080/greeting?name=eugene
--- src/main/java/eugene/controller/HomeController.java
+++ src/main/java/eugene/controller/HomeController.java
@@ -1,7 +1,6 @@
 package eugene.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,11 +8,5 @@ public class HomeController {
     @GetMapping("/home")
     public String home() {
         return "Welcome to Spring Boot";
-    }
-
-    @GetMapping("/greeting")
-    public String greeting(@RequestParam(required = false, defaultValue = "World") String name) {
-        System.out.println("==== get greeting ====");
-        return "Hello, " + name;
     }
 }
